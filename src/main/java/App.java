053b7f7122ef9c47b8d000fdcba28b3588183e9d
Aman Kumar Shah <you@example.com>
2024-04-15 21:31:24 +0530

    1st

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Aman");
+
+//        variables
+
+    //        String
+                    String name = "Aman";
+                    System.out.println("Name :" + name);
+    //          Numbers
+                    int Age = 26;
+                    System.out.println("Age :" + Age);
+    }
+}
---
+++ src/main/java/PrimitiveDataType.java
@@ -0,0 +1,5 @@
+public class PrimitiveDataType {
+    public static void main(String[] args) {
+
+    }
+}
